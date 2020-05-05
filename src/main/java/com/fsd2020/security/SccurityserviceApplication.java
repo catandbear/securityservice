6d9bac02cdcedea4edfc06508d0e56f9b0d478d6
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-05-05 12:36:29 +0800

    commit base code

---
+++ src/main/java/com/fsd2020/security/SccurityserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fsd2020.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccurityserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccurityserviceApplication.class, args);
+	}
+
+}
